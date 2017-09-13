1e0bc9163445817be7e0954d39f42af62b93b4d5
the-inhuman-account <31903083+the-inhuman-account@users.noreply.github.com>
2017-09-12 21:58:18 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -23,4 +23,17 @@ window.onload = () =>
 		id('input').value = 
 			localStorage.getItem('__html_editor_' + id('flname').value);
 	}
+
+	id('input').onkeydown = e =>
+	{
+		if(e.keyCode === 9)
+		{
+			const start = e.target.selectionStart;
+			const end = e.target.selectionEnd;
+			const value = e.target.value;
+			e.target.value = value.substring(0,start) + ' ' + value.substring(end);
+			e.target.selectionStart = e.target.selectionEnd = start + 1;
+			return false;
+		}
+	}
 };
